e836540b6771bc511c63bee76aa05d17454cd108
Mixel-MurP-427 <connor.mullarky@outlook.com>
2024-10-27 17:36:35 -0700

    final key created.

--- Sharp_cipher/constants.py
+++ Sharp_cipher/constants.py
@@ -1,13 +1,6 @@
-import random
-
 sharps = tuple('AEFHIKLMNTVWXYZ')
 rounds = tuple('BCDGJOPQRSU')
 everything = tuple('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-sharps_copy = list(sharps) + list('AEFHIKLMNTVWXYZ'.lower())
-random.shuffle(sharps_copy)
-print(sharps_copy, len(sharps_copy))
-
-
-
-key = ['k', 'w', 'E', 'K', 'y', 'N', 'x', 'v', 'Y', 'L', 'V', 'i', 'h', 'm', 'e', 'F', 'Z', 'f', 'I', 'X', 'H', 'W', 'A', 'M', 'l', 'T', 'n', 'a', 't', 'z']
+encrypted = ('0K', '0W', 'E', 'K', '0Y', 'N', '0X', '0V', 'Y', 'L', 'V', '0I', '0H', '0M', '0E', 'F', 'Z', '0F', 'I', 'X', 'H', 'W', 'A', 'M', '0L', 'T', '0N', '0A', '0T', '0Z')
+decrypted = tuple('abcdefghijklmnopqrstuvwxyz.,! ')
