1d56100674bc1c50dddcdb4ba8c4b5ac1d58277b
Mixel-MurP-427 <connor.mullarky@outlook.com>
2024-10-27 19:32:41 -0700

    Completed nearly all of encrypt.py NOTE: the double round insertor may need some tweaking.

--- Sharp_cipher/encrypt.py
+++ Sharp_cipher/encrypt.py
@@ -11,19 +11,23 @@ output = ''
 for letter in message:
     code_letter = constants.encrypted[constants.decrypted.index(letter)]
     if len(code_letter) == 2:
-#        code_letter = random.choice(constants.rounds) + code_letter[1]
-        code_letter = '0' + code_letter[1]
+        code_letter = random.choice(constants.rounds) + code_letter[1]
+#        code_letter = '0' + code_letter[1]
     output += code_letter
 
+print(len(output))
 
 
+#add bonus double rounds for "fun"
+for i in range(len(output), -1, -1):
+    if random.randint(1, 5) == 1:
+        #assert output[:i] + output[i:] == output
+        output = output[:i] + random.choice(constants.rounds) + random.choice(constants.rounds) + output[i:] #inserts 2 random round chars
 
-#add bonus characters
 
+print(len(output))
 
-
-
-
+#TODO add bonus spaces for more 'fun'
 
 
 
