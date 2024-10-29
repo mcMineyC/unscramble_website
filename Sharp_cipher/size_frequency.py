995a2fb6330ec97c05dc3a0c2c1a317b6ce4fd67
Mixel-MurP-427 <connor.mullarky@outlook.com>
2024-10-28 20:55:17 -0700

    Better sample size for size_frequency

--- Sharp_cipher/size_frequency.py
+++ Sharp_cipher/size_frequency.py
@@ -5,16 +5,21 @@ myFile = open('./Sharp_cipher/long_text_sample.txt', 'r')
 text = myFile.read()
 myFile.close()
 
+myFile = open('./Sharp_cipher/long_text_sample2.txt', 'r', encoding="utf8")
+text2 = myFile.read()
+myFile.close()
+
 word_regex = re.compile(r"[\w']+")
-
-
-all_words = word_regex.findall(text)
 
 output = []
 
-#TODO count num of letters in each word and sort
-for word in all_words:
-    output.append(len(word))
+
+for text_sample in (text, text2):
+
+    all_words = word_regex.findall(text_sample)
+    # count num of letters in each word and sort
+    for word in all_words:
+        output.append(len(word))
 
 
 file_obj = open("./Sharp_cipher/frequency.json", "w")
