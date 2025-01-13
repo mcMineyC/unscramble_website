a8c79c72b234f86915772d77b955b76261bf0ce8
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-01-13 13:55:58 -0800

    update to alphabet binary code thing.

--- base_converter/letters_in_binary.py
+++ base_converter/letters_in_binary.py
@@ -2,8 +2,21 @@
 
 letters = tuple(" abcdefghijklmnopqrstuvwxyz") #includes a space at [0]
 
-message = "Hello world!".lower()
+message = "Connor William Mullarky".lower()
 
-for letter in message:
-    if letter in letters:
-        print(letter + "  " + str(bin(letters.index(letter)))[2:].zfill(5))
+def str_to_bin(stbInput):
+    stbOutput = ""
+    for letter in stbInput:
+        if letter in letters:
+            stbOutput += letter + "  " + str(bin(letters.index(letter)))[2:].zfill(5) + "\n"
+    return stbOutput
+
+print(str_to_bin(message))
+
+def bin_to_str(btsInput): #TODO I don't remember if this actually works
+    btsOutput = ""
+    for start in range(0, len(btsInput), 5):
+        setO5 = btsInput[start, start+5]
+        print(setO5)
+
+#bin_to_str(str(111110001101111011100111001111110010))
