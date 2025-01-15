d25d01c230e3bbfef1d0e62c5ff62f79d98312fc
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-01-14 20:58:26 -0800

    Minor bug fix: added zfill 128 to binary_hash.py

--- base_converter/binary_hash.py
+++ base_converter/binary_hash.py
@@ -5,7 +5,7 @@ message = md5("Hello world!".encode()).hexdigest().upper()
 
 print(message)
 
-myBinary = base_to_base(message, hexadecimal, binary)
+myBinary = base_to_base(message, hexadecimal, binary).zfill(128)
 
 for i in range(0, len(myBinary), 8): # prints one byte at a time.
     print(myBinary[i:i+8])
