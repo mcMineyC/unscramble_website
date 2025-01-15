93d330cdd2a9c5ef5d432b97a714fb5fed3911cb
Mixel-MurP-427 <connor.mullarky@outlook.com>
2025-01-14 20:13:07 -0800

    Created program to print binary of MD5 hash.

---
+++ base_converter/binary_hash.py
@@ -0,0 +1,13 @@
+from base_after_base import hexadecimal, binary, base_to_base
+from hashlib import md5
+
+message = md5("Hello world!".encode()).hexdigest().upper()
+
+print(message)
+
+myBinary = base_to_base(message, hexadecimal, binary)
+
+for i in range(0, len(myBinary), 8): # prints one byte at a time.
+    print(myBinary[i:i+8])
+
+assert len(myBinary) == 128 #an MD5 hash always returns 128 bits
--- base_converter/letters_in_binary.py
+++ base_converter/letters_in_binary.py
@@ -2,7 +2,7 @@
 
 letters = tuple(" abcdefghijklmnopqrstuvwxyz") #includes a space at [0]
 
-message = "Connor William Mullarky".lower()
+message = "Billy Bob Joe".lower()
 
 def str_to_bin(stbInput):
     stbOutput = ""
@@ -11,12 +11,8 @@ def str_to_bin(stbInput):
             stbOutput += letter + "  " + str(bin(letters.index(letter)))[2:].zfill(5) + "\n"
     return stbOutput
 
-print(str_to_bin(message))
-
 def bin_to_str(btsInput): #TODO I don't remember if this actually works
     btsOutput = ""
     for start in range(0, len(btsInput), 5):
         setO5 = btsInput[start, start+5]
         print(setO5)
-
-#bin_to_str(str(111110001101111011100111001111110010))
